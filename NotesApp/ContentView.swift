e5d2ff0e677cd54abb709bf451f8e1972bd353d9
Lais Godinho <47223072+laisbastosbg@users.noreply.github.com>
2024-11-11 16:54:40 -0300

    :construction: feat: adiciona funções de rascunho com UserDefaults

--- NotesApp/ContentView.swift
+++ NotesApp/ContentView.swift
@@ -44,23 +44,27 @@ struct ContentView: View {
                     }
                 }
             }
-            
             .navigationTitle("Notas salvas")
-            
-            .sheet(isPresented: $isShowingSheet, onDismiss: { }) {
+            .sheet(isPresented: $isShowingSheet, onDismiss: { saveDraftToUserDefaults() }) {
                 List {
                     Section(header:
                         Text("Nova nota")
                     ) {
                         TextField("Título", text: $draft.title)
-                        TextField("Lembrar...", text: $draft.content)
+                            .onChange(of: draft.title) {
+                                saveDraftToUserDefaults()
+                            }
+                        TextField("Lembrar...", text: $draft.content, axis: .vertical)
+                            .onChange(of: draft.content) {
+                            saveDraftToUserDefaults()
+                        }
                         HStack {
                             Spacer()
                             Button(action: {
                                 if draft.isFilled() {
                                     let newNote = Note(title: draft.title, content: draft.content)
-                                    
                                     context.insert(newNote)
+                                    clearDraft()
                                     isShowingSheet = false
                                 }
                             }) {
@@ -72,8 +76,20 @@ struct ContentView: View {
             }
         }
     }
-}
-
-#Preview {
-    ContentView()
+    
+    private func saveDraftToUserDefaults() {
+        UserDefaults.standard.set(draft.title, forKey: "draftTitle")
+        UserDefaults.standard.set(draft.content, forKey: "draftContent")
+    }
+    
+    private func loadDraftFromUserDefaults() {
+        draft.title = UserDefaults.standard.string(forKey: "draftTitle") ?? ""
+        draft.content = UserDefaults.standard.string(forKey: "draftContent") ?? ""
+    }
+    
+    private func clearDraft() {
+        draft = NoteDraft()
+        UserDefaults.standard.removeObject(forKey: "draftTitle")
+        UserDefaults.standard.removeObject(forKey: "draftContent")
+    }
 }
